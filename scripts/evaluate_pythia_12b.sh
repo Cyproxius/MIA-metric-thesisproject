10bc3fc03f167de2b02e21d846a7a490e9716b48
Cyproxius <maris@vrijheid.net>
2024-07-28 14:26:25 +0200

    added run scripts

---
+++ scripts/evaluate_pythia_12b.sh
@@ -0,0 +1,5 @@
+virtualenv --python python3 --system-site-packages $temp/env
+$temp/env/bin/pip install transformers torch tqdm numpy datasets accelerate matplotlib
+$temp/env/bin/python run.py --model "EleutherAI/pythia-12b" --dataset "swj0419/WikiMIA" --split_name "WikiMIA_length128"
+$temp/env/bin/python run.py --model "EleutherAI/pythia-12b" --dataset "Cyproxius/GutenbergMIA_temporal" --split_name "GutenbergMIA_length128"
+rm -rf $temp/env
---
+++ scripts/grid_search_pythia_6_9b.sh
@@ -0,0 +1,4 @@
+virtualenv --python python3 --system-site-packages $temp/env
+$temp/env/bin/pip install transformers torch tqdm numpy datasets accelerate matplotlib
+$temp/env/bin/python run.py --model "EleutherAI/pythia-6.9b" --dataset "swj0419/WikiMIA" --split_name "WikiMIA_length128" --learning_rates [0.0000001, 0.0000005, 0.000001, 0.000005, 0.00001] --batch_sizes [1,8,16] --unlearning_steps [4]
+rm -rf $temp/env
