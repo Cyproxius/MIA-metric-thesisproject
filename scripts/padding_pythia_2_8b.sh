cc6b17479fc759b68ed6bec1947334eea2f4e7cb
Maris Koopmans <62873987+Cyproxius@users.noreply.github.com>
2024-07-31 11:16:59 +0200

    Remove padding_pythia_2_8b.sh

--- scripts/padding_pythia_2_8b.sh
+++ scripts/padding_pythia_2_8b.sh
@@ -1,5 +1,1 @@
-virtualenv --python python3 --system-site-packages $temp/env
-$temp/env/bin/pip install transformers torch tqdm numpy datasets accelerate matplotlib
-$temp/env/bin/python run.py --model "EleutherAI/pythia-6.9b" --dataset "swj0419/WikiMIA" --split_name "WikiMIA_length128" --threshold 0
-$temp/env/bin/python run.py --model "EleutherAI/pythia-6.9b" --dataset "Cyproxius/GutenbergMIA_temporal" --split_name "GutenbergMIA_length128" --threshold 0
-rm -rf $temp/env
+
